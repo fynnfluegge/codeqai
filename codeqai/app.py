c2a7a75f8748e2ff2c3a5f12f120bda11083d78d
fynnfluegge <fynnfluegge@gmx.de>
2023-09-25 14:57:54 +0200

    Add sync functionality and track changed files
    Added the ability to sync the codebase with the latest changes and track the changed files. The sync action will compare the current commit with the previous commit and retrieve the list of chang
    ed files. Further implementation is required to update the files accordingly.

--- codeqai/app.py
+++ codeqai/app.py
@@ -1,5 +1,6 @@
 import argparse
 import os
+import subprocess
 
 from langchain.chains import ConversationalRetrievalChain
 from langchain.memory import ConversationSummaryMemory
@@ -9,7 +10,7 @@ from yaspin import yaspin
 
 from codeqai import codeparser, repo, utils
 from codeqai.config import (create_cache_dir, create_config, get_cache_path,
-                            load_config)
+                            load_config, save_config)
 from codeqai.constants import EmbeddingsModel, LllmHost
 from codeqai.embeddings import Embeddings
 from codeqai.llm import LLM
@@ -20,7 +21,7 @@ def run():
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "action",
-        choices=["search", "chat", "configure"],
+        choices=["search", "chat", "configure", "sync"],
         help="Action to perform. 'search' will semantically search the codebase. 'chat' will chat with the codebase.",
     )
     args = parser.parse_args()
@@ -36,7 +37,8 @@ def run():
     except FileNotFoundError:
         config = create_config()
 
-    repo_name = repo.get_git_root(os.getcwd()).split("/")[-1]
+    current_repo = repo.get_git_root(os.getcwd())
+    repo_name = current_repo.split("/")[-1]
 
     # init cache
     create_cache_dir()
@@ -60,12 +62,29 @@ def run():
             embeddings=embeddings_model.embeddings,
             documents=documents,
         )
+        config[f"{repo_name}-commit"] = current_repo.head.commit.hexsha
+        save_config(config)
         spinner.stop()
     else:
         spinner = yaspin(text="💾 Loading vector store...", color="green")
         spinner.start()
         vector_store = VectorStore(repo_name, embeddings=embeddings_model.embeddings)
         spinner.stop()
+
+    if args.action == "sync":
+        git_command = [
+            "git",
+            "diff",
+            "--name-only",
+            config[f"{repo_name}-commit"],
+            current_repo.head.commit.hexsha,
+        ]
+        result = subprocess.run(
+            git_command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+        )
+        if result.returncode == 0:
+            changed_files = result.stdout.splitlines()
+            # TODO update files
 
     llm = LLM(
         llm_host=LllmHost[config["llm-host"].upper().replace("-", "_")],

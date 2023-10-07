e0705db16d0b1e476e620c18c5c8b54d6028aaac
Fynn Flügge <fynnfluegge@gmx.de>
2023-10-07 13:21:26 +0200

    feat: add c, cpp, csharp, yaml, md, rst support (#7)
    Closes #5

--- codeqai/treesitter/__init__.py
+++ codeqai/treesitter/__init__.py
@@ -1,4 +1,8 @@
-from codeqai.treesitter.treesitter import Treesitter, TreesitterMethodNode
+from codeqai.treesitter.treesitter import (Treesitter,
+                                                   TreesitterMethodNode)
+from codeqai.treesitter.treesitter_c import TreesitterC
+from codeqai.treesitter.treesitter_cpp import TreesitterCpp
+from codeqai.treesitter.treesitter_cs import TreesitterCsharp
 from codeqai.treesitter.treesitter_go import TreesitterGo
 from codeqai.treesitter.treesitter_java import TreesitterJava
 from codeqai.treesitter.treesitter_js import TreesitterJavascript
---
+++ codeqai/treesitter/treesitter_c.py
@@ -0,0 +1,26 @@
+import tree_sitter
+
+from codeqai.constants import Language
+from codeqai.treesitter.treesitter import Treesitter
+from codeqai.treesitter.treesitter_registry import TreesitterRegistry
+
+
+class TreesitterC(Treesitter):
+    def __init__(self):
+        super().__init__(Language.C, "function_definition", "identifier", "comment")
+
+    def _query_method_name(self, node: tree_sitter.Node):
+        if node.type == self.method_declaration_identifier:
+            for child in node.children:
+                # if method returns pointer, skip pointer declarator
+                if child.type == "pointer_declarator":
+                    child = child.children[1]
+                if child.type == "function_declarator":
+                    for child in child.children:
+                        if child.type == self.method_name_identifier:
+                            return child.text.decode()
+        return None
+
+
+# Register the TreesitterJava class in the registry
+TreesitterRegistry.register_treesitter(Language.C, TreesitterC)
